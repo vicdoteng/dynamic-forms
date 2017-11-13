4443aca2279744e1257cafb0a9742037fe65a9d6
viccordium <victor.eng@cordium.com>
2017-11-12 19:02:34 -0500

    working dynamic fields for text input and textarea. check readme!

--- src/containers/FormContainer.js
+++
@@ -1,7 +0,0 @@
-import { Component } from 'react';
-
-class FormComponent extends Component {
-    render() {
-        <div>Form!</div>
-    }
-}
--- src/services/form-api.js
+++ src/services/form-api.js
@@ -1,68 +1,61 @@
-const panel1Fields = [
+const formConfig = [
     {
+        id: 'name',
         fieldType: 'text',
         dataType: 'string',
         label: 'Name',
         required: true,
 
-        maxLength: 50,
-        default: 'Enter your name'
+        config: {
+            maxLength: 50,
+            placeholder: 'Enter your name'
+        }
     },
     {
+        id: 'desc',
         fieldType: 'textarea',
         dataType: 'string',
         label: 'Description',
         required: false,
 
-        rows: 12,
-        cols: 60
-    }
-]
-
-const panel2Fields = [
+        config: {
+            rows: 12,
+            cols: 60
+        }
+    },
     {
+        id: 'rating',
         fieldType: 'pick-one',
         dataType: 'number',
         label: 'Rating',
         required: true,
 
-        options: {
-            1: 'One',
-            2: 'Two',
-            3: 'Three',
-            4: 'Four',
-            5: 'Five'
+        config: {
+            options: {
+                1: 'One',
+                2: 'Two',
+                3: 'Three',
+                4: 'Four',
+                5: 'Five'
+            }
         }
     },
     {
+        id: 'extras',
         fieldType: 'pick-many',
         dataType: 'array string',
         label: 'Extras',
         required: false,
 
-        options: {
-            batteries: 'Extra Batteries',
-            cables: 'Cables',
-            manual: 'Manual'
+        config: {
+            options: {
+                batteries: 'Extra Batteries',
+                cables: 'Cables',
+                manual: 'Manual'
+            }
         }
     },
-
 ]
-
-const formConfig = {
-    panels: [
-        {
-            id: 1,
-            order: 1,
-            fields: panel1Fields
-        },
-        {
-            id: 2,
-            order: 2,
-            fields: panel2Fields
-        }
-    ]
-}
 
 function getFormJson() {
     return JSON.stringify(formConfig);
